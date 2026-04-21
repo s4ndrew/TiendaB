02b47e4c16964b9ddfae059230fc291cac5b714d
Andrew Chiroque <andreschiroque.07@gmail.com>
2026-04-21 01:59:12 -0500

    implmentacion de js y html

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,3 @@
-
-
 /*
 function fetchData1(){
     fetch("http://localhost:8089/tiendaMia/ventas")
@@ -14,45 +12,43 @@ fetchData1();
 */
 
 async function dataCategorias() {
-    try {
-        let response = await fetch ("http://localhost:8089/tiendaMia/categorias")
-        let data = await response.json();
-        //console.log(data);
-        const container = document.getElementById("list")
-        
-        container.innerHTML = "";
+  try {
+    let response = await fetch("http://localhost:8089/tiendaMia/categorias");
+    let data = await response.json();
+    //console.log(data);
+    const container = document.getElementById("list");
 
-         data.forEach(categoria => {
-            const li = document.createElement('li')
-            
-            li.textContent = `${categoria.idCategoria} ${categoria.nombre}`
-            container.appendChild(li)
-         });
-    } catch (error) {
-        console.log(error);
-    }
+    container.innerHTML = "";
+
+    data.forEach((categoria) => {
+      const li = document.createElement("li");
+
+      li.textContent = `${categoria.idCategoria} ${categoria.nombre}`;
+      container.appendChild(li);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-dataCategorias()
-
-
+//dataCategorias()
 
 async function dataClientes() {
-    try {
-        let response = await fetch("http://localhost:8089/tiendaMia/clientes")
-        let data = await response.json()
-        
-        let cl = document.getElementById("clientes")
-        cl.innerHTML="";
-        data.forEach(clientes => {
-            let li = document.createElement("li")
-            li.textContent = clientes.nombre
-            cl.appendChild(li)
-        });
-    } catch (error) {
-        console.log("error" + error);
-    }
+  try {
+    let response = await fetch("http://localhost:8089/tiendaMia/clientes");
+    let data = await response.json();
+
+    let cl = document.getElementById("clientes");
+    cl.innerHTML = "";
+    data.forEach((clientes) => {
+      let li = document.createElement("li");
+      li.textContent = clientes.nombre;
+      cl.appendChild(li);
+    });
+  } catch (error) {
+    console.log("error" + error);
+  }
 }
-dataClientes()
+//dataClientes()
 //Bucle con varias peticiones
 /*
 const urls = [
@@ -75,3 +71,81 @@ async function fecthName() {
     }
 }
 fecthName() */
+
+function sendHTTPRequest(method, url, data) {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-type": "applicaction/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+//CATEGORIAS
+const form = document.getElementById("form");
+//const selectCategoria = document.createElement("select");
+const contenedorCategoria = document.getElementById("opcionesCategorias")
+const contenerdorProductos = document.getElementById("opcionesProductos")
+const cuadroDePrecio = document.getElementById("precioProducto")
+
+async function fecthCategorias() {
+  const responseData = await sendHTTPRequest(
+    "GET",
+    "http://localhost:8089/tiendaMia/categorias",
+  );
+  console.log(responseData);
+  const listaCategorias = responseData;
+  //CREACION DE CONTENEDOR
+
+  for (const categoria of listaCategorias) {
+    const optionCategoria = document.createElement("option");
+    optionCategoria.innerHTML = "";
+    optionCategoria.value = categoria.idCategoria;
+    optionCategoria.textContent = categoria.nombre;
+    contenedorCategoria.appendChild(optionCategoria);
+  }
+
+  fetchProductos();
+}
+
+//PRODUCTOS  
+//const selectProducto = document.createElement("select");
+async function fetchProductos() {
+  const responseData = await sendHTTPRequest(
+    "GET",
+    "http://localhost:8089/tiendaMia/productos",
+  );
+  //console.log(responseData);
+
+  //const listaProductos = responseData.filter(p => p.idCategoria == idCategoriaSeleccionada);
+  const listaProductos = responseData;
+  contenerdorProductos.innerHTML = ""
+  for (const producto of listaProductos) {
+    if (contenedorCategoria.value == producto.idCategoria) {
+      const optionProducto = document.createElement("option");
+      optionProducto.value = producto.idProducto;
+      optionProducto.textContent = producto.nombre;
+      optionProducto.setAttribute("data-precio", producto.precio);
+      contenerdorProductos.appendChild(optionProducto);
+    }
+  }
+  precio();
+}
+
+function precio(params) {
+    const precioElegido = contenerdorProductos.options[contenerdorProductos.selectedindex]    
+    if (precioElegido) {
+          cuadroDePrecio.value = precioElegido.getAttribute("data-precio");
+    } else {
+          cuadroDePrecio.value = ""
+    }
+
+  }
+
+ contenedorCategoria.onchange = fetchProductos
+ contenerdorProductos.onchange = precio
+//fetchProductos();
+fecthCategorias();
